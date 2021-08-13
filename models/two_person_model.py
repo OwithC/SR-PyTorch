4e29a6a50ca635f1219f7fb828fad8f218e78324
b._.obo <76933244+boookk@users.noreply.github.com>
2021-08-13 14:34:37 +0900

    Add vgg model

--- models/two_person_model.py
+++ models/two_person_model.py
@@ -31,3 +31,35 @@ class resnet(nn.Module):
         x = self.fc3(x)
 
         return x
+    
+    
+class vgg(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = models.vgg16(pretrained=True)
+        self.features = nn.Sequential(*list(self.model.children())[:-1])
+
+        # class : friends, family, couple, professional, commercial, no_relation
+        self.num_classes = 6
+
+        self.fc = nn.Sequential(
+            nn.Linear(50176, 4096, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Linear(4096, 4096, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Linear(4096, self.num_classes, bias=True)
+        )
+
+    def forward(self, x1, x2):
+        x1 = self.features(x1)
+        x2 = self.features(x2)
+        
+        x = torch.cat((x1, x2), dim=1)
+
+        x = x.view(x.shape[0], -1)
+
+        x = self.fc(x)
+
+        return x
