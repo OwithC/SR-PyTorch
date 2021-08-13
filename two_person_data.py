bd36e56de9bcc1cdd2b1c6ae44eba327503fb7cb
b._.obo <76933244+boookk@users.noreply.github.com>
2021-08-13 15:08:59 +0900

    Update two_person_data.py

--- two_person_data.py
+++ two_person_data.py
@@ -35,7 +35,7 @@ class PISCDataset(torch.utils.data.Dataset):
         transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
         return transform
