8e282c937faef31bb24249e749cc8f572b949cdf
b._.obo <76933244+boookk@users.noreply.github.com>
2021-08-27 08:52:26 +0900

    Update two_person_data.py

--- two_person_data.py
+++ two_person_data.py
@@ -1,35 +1,35 @@
-import torch
+from torch.utils.data import Dataset
 from torchvision import transforms
 
-from glob import glob
 from PIL import Image, ImageFile
-import json
-import os
+from pathlib import Path
+from itertools import chain
 
 
-class PISCDataset(torch.utils.data.Dataset):
-    def __init__(self, data_path, id_list):
+class PISCDataset(Dataset):
+    def __init__(self, path):
         ImageFile.LOAD_TRUNCATED_IMAGES = True
-        self.path = data_path
+        self.path = Path(path).absolute()
         self.transforms = self.get_transforms()
-        self.img_idx = -1
-        self.item = []
-
-        ########## Path modification required ##########
-        # Get Full Relation
-        with open('/PATH/PISC/relationship.json') as j:
-            self.relationship = json.load(j)
-
-        self.ids = id_list
-        self.relations = [self.relationship[id] for id in self.ids]
-        self.length = sum([len(r) for r in self.relations])
+        self.classes = ['friends', 'family', 'couple', 'professional', 'commercial', 'no_relation']
+        self.dir = [list((self.path / c).iterdir()) for c in self.classes]
+        self.dir = sorted(chain(*self.dir))
 
     def __getitem__(self, idx):
-        item = self.get_item()
-        return item
+        relation = str(self.dir[idx].parent).split('/')[-1]
+        img_path = [str(path) for path in self.dir[idx].glob('*.png')]
+
+        person1 = Image.open(img_path[0]).convert('RGB')
+        person2 = Image.open(img_path[1]).convert('RGB')
+
+        if self.transforms is not None:
+            person1 = self.transforms(person1)
+            person2 = self.transforms(person2)
+            
+        return person1, person2, self.classes.index(str(relation))
 
     def __len__(self):
-        return self.length
+        return len(self.dir)
 
     def get_transforms(self):
         transform = transforms.Compose([
@@ -39,39 +39,3 @@ class PISCDataset(torch.utils.data.Dataset):
         ])
 
         return transform
-
-    def get_item(self):
-        '''
-
-        :return:
-        두 사람의 각 영역에 대한 이미지, 두 사람의 관계에 대한 라벨
-        '''
-        if not self.item:
-            self.img_idx += 1
-
-            # 하나의 이미지에서 잘라서 저장한 사람 영역의 이미지를 리스트에 저장
-            people = []
-
-            paths = glob(os.path.join(self.path + self.ids[self.img_idx].zfill(5)) + '/*.png')
-            paths.sort()
-
-            for path in paths:
-                people.append(Image.open(path).convert('RGB'))
-
-            # 주석에 따라 두 사람의 이미지와 관계를 리스트에 저장
-            self.item = []
-            for i, (x, y) in enumerate(self.relations[self.img_idx].items()):
-                a, b = x.split()
-                a, b = int(a), int(b)
-                person1 = people[a - 1]
-                person2 = people[b - 1]
-                if self.transforms is not None:
-                    person1 = self.transforms(person1)
-                    person2 = self.transforms(person2)
-
-                self.item.append([person1, person2, y-1])
-
-        if self.img_idx >= len(self.relations) - 1:
-            self.img_idx = -1
-
-        return self.item.pop(0)
