27a9372d875fdb88c382755b6bfff0cdf052961e
b._.obo <76933244+boookk@users.noreply.github.com>
2021-08-27 12:15:49 +0900

    Update two_person_main.py

--- two_person_main.py
+++ two_person_main.py
@@ -1,30 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import random_split, DataLoader, WeightedRandomSampler
 import torch.backends.cudnn as cudnn
 
 import os
-import json
 from tqdm import tqdm
 
-from models.two_person_model import resnet, vgg
+from models.two_person_model import resnet
 from two_person_data import PISCDataset
-
-
-def get_splitList():
-    with open('/PATH/PISC/relationship_split/relation_trainidx.json') as j:
-        train_list = json.load(j)
-
-    with open('/PATH/PISC/relationship_split/relation_validx.json') as j:
-        val_list = json.load(j)
-
-    with open('/PATH/PISC/relationship_split/relation_testidx.json') as j:
-        test_list = json.load(j)
-
-    test_list += val_list
-
-    return train_list, test_list
 
 
 if __name__ == '__main__':
@@ -34,16 +18,46 @@ if __name__ == '__main__':
     cudnn.enabled = True
     cudnn.benchmark = True
 
-    # Import Data List
-    train_list, test_list = get_splitList()
+    datasets = PISCDataset('./crop')
 
-    train_datasets = PISCDataset('/PATH/PISC/image/data/preprocessed/train/', train_list)
-    test_datasets = PISCDataset('/PATH/PISC/image/data/preprocessed/test/', test_list)
+    train_size = int(len(datasets) * 0.8)
+    test_size = len(datasets) - train_size
+    train_dataset, test_dataset = random_split(datasets, [train_size, test_size])
 
-    train_loader = DataLoader(train_datasets, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_datasets, batch_size=64, shuffle=False)
+    # Train, Test 클래스 별로 데이터 갯수 카운팅 / Train data label 리스트로 저장
+    train_cnt = [0] * 6
+    test_cnt = [0] * 6
+    labels = []
+    for i, datas in enumerate(train_dataset):
+        x1, x2, y = datas
+        train_cnt[y] += 1
+        labels.append(y)
+    else:
+        print(f'Train Data : {sum(train_cnt)} : {train_cnt}')
 
-    ############# You can change the model. ###########
+    for i, datas in enumerate(test_dataset):
+        x1, x2, y = datas
+        test_cnt[y] += 1
+    else:
+        print(f'Test Data : {sum(test_cnt)}/* : {test_cnt}')
+
+    num_samples = sum(train_cnt)
+
+    class_weights = [num_samples / train_cnt[i] for i in range(len(train_cnt))]
+    weights = [class_weights[labels[i]] for i in range(int(num_samples))]
+    sampler = WeightedRandomSampler(torch.DoubleTensor(weights), int(num_samples))
+    
+    train_loader = DataLoader(train_dataset, batch_size=64, sampler=sampler)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    
+    sam_cnt = [0] * 6
+    for i, datas in enumerate(train_loader):
+        x1, x2, labels = datas
+        for label in labels:
+            sam_cnt[label] += 1
+    else:
+        print(f'Sampling Train Data : {sum(sam_cnt)} : {sam_cnt}')
+        
     model = resnet()
 
     # Using Multiple GPU
@@ -51,7 +65,6 @@ if __name__ == '__main__':
     if (device.type == 'cuda') and (torch.cuda.device_count() > 1):
         print('Multi GPU activate')
         model = nn.DataParallel(model)
-
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
@@ -60,7 +73,8 @@ if __name__ == '__main__':
     model.train()
     for epoch in tqdm(range(num_epochs)):
         running_loss = 0.0
-        for i, (x1, x2, y) in enumerate(train_loader):
+        for i, datas in enumerate(train_loader):
+            x1, x2, y = datas
             x1 = x1.to(device)
             x2 = x2.to(device)
             y = y.to(device)
@@ -74,68 +88,61 @@ if __name__ == '__main__':
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 100 == 99:
-                print('Epoch {:d}/{:d} | Batch {:d}/{:d} | Loss {:f}'.format(epoch + 1, num_epochs,
-                                                                             i + 1, len(train_loader),
-                                                                             running_loss / 100))
-                running_loss = 0.0
+        else:
+            print('Epoch {:d}/{:d} | Batch {:d}/{:d} | Loss {:f}'.format(epoch + 1, num_epochs,
+                                                             i + 1, len(train_loader),
+                                                             running_loss / 100))
 
         if epoch % save_epoch == save_epoch - 1:
             save_file = os.path.join('./checkpoint/', str(epoch + 1) + '.pth')
             torch.save(model.state_dict(), save_file)
 
-    # Evaluation
-    for j in range(int(num_epochs / save_epoch)):
-        num_model = save_epoch * (j + 1)
+    ''' Validation '''
+    save_file = os.path.join('./checkpoint/', str(num_epochs) + '.pth')
+    model = resnet()
 
-        ########### Modify the checkpoint directory path ###########
-        save_file = os.path.join('./checkpoint/', str(num_model) + '.pth')
+    load_params = torch.load(save_file)
+    new_params = model.state_dict().copy()
+    for j in load_params:
+        jj = j.split('.')
+        if jj[0] == 'module':
+            new_params['.'.join(jj[1:])] = load_params[j]
+        else:
+            new_params[j] = load_params[j]
+    model.load_state_dict(new_params)
 
-        ############# You can change the model. ###########
-        model = resnet()
-        model = model.to(device)
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    model = model.to(device)
 
-        loaded_params = torch.load(save_file)
-        new_params = model.state_dict().copy()
-        for i in loaded_params:
-            i_parts = i.split('.')
-            if i_parts[0] == 'module':
-                new_params['.'.join(i_parts[1:])] = loaded_params[i]
-            else:
-                new_params[i] = loaded_params[i]
-        model.load_state_dict(new_params)
+    model.eval()
 
-        if torch.cuda.device_count() > 1:
-            model = nn.DataParallel(model)
+    correct = 0.0
+    with torch.no_grad():
+        for k, (x1, x2, y) in enumerate(train_loader):
+            x1 = x1.to(device)
+            x2 = x2.to(device)
+            y = y.to(device)
 
-        model.eval()
+            outputs = model(x1, x2)
 
-        correct = 0.0
-        with torch.no_grad():
-            for k, (x1, x2, y) in enumerate(train_loader):
-                x1 = x1.to(device)
-                x2 = x2.to(device)
-                y = y.to(device)
+            _, predicted = torch.max(outputs, 1)
+            correct += torch.sum(predicted == y.data)
 
-                outputs = model(x1, x2)
+    epoch_acc = 100 * correct / len(train_loader.dataset)
+    print('Train Accuracy : {:.2f}%'.format(epoch_acc))
 
-                _, predicted = torch.max(outputs, 1)
-                correct += torch.sum(predicted == y.data)
+    correct = 0.0
+    with torch.no_grad():
+        for k, (x1, x2, y) in enumerate(test_loader):
+            x1 = x1.to(device)
+            x2 = x2.to(device)
+            y = y.to(device)
 
-        epoch_acc = 100 * correct / len(train_loader.dataset)
-        print('Train Accuracy({:d}) {:.2f}%'.format(num_model, epoch_acc))
+            outputs = model(x1, x2)
 
-        correct = 0.0
-        with torch.no_grad():
-            for k, (x1, x2, y) in enumerate(test_loader):
-                x1 = x1.to(device)
-                x2 = x2.to(device)
-                y = y.to(device)
+            _, predicted = torch.max(outputs, 1)
+            correct += torch.sum(predicted == y.data)
 
-                outputs = model(x1, x2)
-
-                _, predicted = torch.max(outputs, 1)
-                correct += torch.sum(predicted == y.data)
-
-        epoch_acc = 100 * correct / len(test_loader.dataset)
-        print('Test Accuracy({:d}) {:.2f}%'.format(num_model, epoch_acc))
+    epoch_acc = 100 * correct / len(test_loader.dataset)
+    print('Test Accuracy : {:.2f}%'.format(epoch_acc))
